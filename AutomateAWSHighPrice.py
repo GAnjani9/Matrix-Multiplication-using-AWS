682fa82bb959a1cc1121142bd9df72aa3d5587b7
ashubsp <ashubsp@gmail.com>
2021-09-10 20:11:05 +0530

    Large Matrix Mutliplication using AWS
    -Python File will automatically create Amazon Ubuntu fee tier instance
    based on image id and instance type provided
    -It will execute set of commands hardocoded in the code
    -After execution of code, it will stop all running instances and
    terminate it thereafter
    -Matrix of 5000 x 5000 being multiplied which is of complexity N^3
    -Running matrix multiplication in two instances, one is t2.micro which
    has low specs - 1vCPUs, 2.5 GHz and 1 GB memory
    -Other one is c4.4xlarge which has specs - 16 vCPUs, 2.9 GHz and 30 GB
    memory

---
+++ AutomateAWSHighPrice.py
@@ -0,0 +1,95 @@
+import boto3
+import subprocess
+import os
+import time
+from botocore.exceptions import ClientError
+#import resources
+
+ec2_client = boto3.client('ec2')
+
+response = ec2_client.describe_vpcs()
+vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+print(vpc_id)
+
+# try:
+#     response = ec2_client.create_security_group(GroupName='HPSG',
+#                                                 Description='DESCRIPTION',
+#                                                 VpcId=vpc_id)
+#     print(response['GroupId'])
+#     security_group_id = response['GroupId']
+#     print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+#     security_group_id=str(security_group_id)
+
+#     data = ec2_client.authorize_security_group_ingress(GroupId=security_group_id,
+#                                                         IpPermissions=[
+#                                                             {'IpProtocol': 'tcp',
+#                                                              'FromPort': 80,
+#                                                              'ToPort': 80,
+#                                                              'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+#                                                             {'IpProtocol': 'tcp',
+#                                                              'FromPort': 22,
+#                                                              'ToPort': 22,
+#                                                              'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+#                                                         ])
+#     print('Ingress Successfully Set %s' % data)
+# except ClientError as e:
+#     print(e)
+
+
+
+aws_key = 'AKIAT22APHAQZIALILWJ'
+aws_secret = '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'
+
+ec2 = boto3.resource('ec2', aws_access_key_id=aws_key,
+                     aws_secret_access_key=aws_secret,region_name='ap-south-1')
+
+# Launching new instances requires an image ID and the number of instances to launch.
+# It can also take several optional parameters, such as the instance type and security group:
+# inst = ec2.create_instances(ImageId='ami-0c1a7f89451184c8b', MinCount=1, MaxCount=1, InstanceType='c4.4xlarge',KeyName='CloudComputingProject',
+# 	NetworkInterfaces=[{ 'DeviceIndex': 0, 'AssociatePublicIpAddress': True,'Groups': [security_group_id]}])
+# insta = inst[0]
+
+# # # Wait for the instance to enter the running state
+# insta.wait_until_running()
+# # # Reload the instance attributes
+# insta.load()
+
+# time.sleep(60)
+instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+# pdn=[]
+# for instance in instances:
+# 	pdn.append(instance.public_dns_name)
+# print(pdn)
+# pdn[0]=str("ubuntu@"+pdn[0])
+
+
+# for instance in instances:
+#     cmd1 = "ssh -i CloudComputingProject.pem " + "ubuntu@" + str(instance.public_dns_name)
+#     cmdList = [cmd1 + " mkdir AnjaniCloud",
+#                cmd1 + " sudo apt-get update",
+#                cmd1 + " sudo apt-get install g++",
+#                " scp -i CloudComputingProject.pem MatrixMutliplication.cpp " + "ubuntu@" + str(instance.public_dns_name) + ":~/AnjaniCloud/",
+#                cmd1 + " g++ AnjaniCloud/MatrixMutliplication.cpp",
+#                cmd1 + " ./a.out"]
+#     for cmd in cmdList:
+#         returned_value = os.system(cmd)  # returns the exit code in unix
+#         print('returned value:', returned_value)
+# call = subprocess.check_output(["ssh", "-i", "CloudComputingProject.pem", pdn[0]], stderr=subprocess.PIPE)
+# for line in call:
+#     print(call.stdout)
+
+# returned_value1 = os.system(cmd1)  # returns the exit code in unix
+# print('returned value:', returned_value1)
+
+# call = subprocess.check_output(["exit"], stderr=subprocess.PIPE)
+# time.sleep(10)
+
+
+
+
+ids=[]
+instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+for instance in instances:
+	ids.append(instance.id)
+ec2.instances.filter(InstanceIds=ids).stop()
+ec2.instances.filter(InstanceIds=ids).terminate()
---
+++ MatrixMutliplication.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+
+// Edit MACROs here, according to your Matrix Dimensions for mat1[SIZE][SIZE] and mat2[SIZE][SIZE]
+#define SIZE 2000     
+
+void mulMat(unsigned long int **mat1, unsigned long int **mat2) {
+    unsigned long int** rslt = new unsigned long int*[SIZE];
+
+    for (unsigned long int i = 0; i < SIZE; i++)
+    	rslt[i] = new unsigned long int[SIZE];
+ 
+    std::cout<< "Multiplication of given two matrices is:\n\n";
+ 
+    for (unsigned long int i = 0; i < SIZE; i++) {
+        for (unsigned long int j = 0; j < SIZE; j++) {
+            rslt[i][j] = 0;
+ 
+            for (unsigned long int k = 0; k < SIZE; k++) {
+                rslt[i][j] += mat1[i][k] * mat2[k][j];
+            }
+ 
+            // std::cout<< rslt[i][j] <<" ";
+        }
+ 
+        // std::cout<<"\n";
+    }
+}
+
+
+int main()
+{
+    // Square Matrices
+    // SIZE = 4, SIZE = 4 and SIZE = 4, SIZE = 4 (Update these values in MACROs)
+    unsigned long int** mat1 = new unsigned long int*[SIZE];
+    unsigned long int** mat2 = new unsigned long int*[SIZE];
+
+    for (unsigned long int i = 0; i < SIZE; i++)
+    	mat1[i] = new unsigned long int[SIZE];
+
+    for (unsigned long int i = 0; i < SIZE; i++)
+    	mat2[i] = new unsigned long int[SIZE];
+        /*
+    // Rectangular Matrices
+    // SIZE = 3, SIZE = 4 and SIZE = 4, SIZE = 3 (Update these values in MACROs)
+    unsigned long int mat1[SIZE][SIZE] = {
+            {1, 1, 1, 1},
+            {2, 2, 2, 2},
+            {3, 3, 3, 3}
+    };
+ 
+    unsigned long int mat2[SIZE][SIZE] = {
+            {1, 1, 1},
+            {2, 2, 2},
+            {3, 3, 3},
+            {4, 4, 4}
+    };
+    */
+
+    for (unsigned long int i = 0; i < SIZE; i++)
+    	for (unsigned long int j = 0; j < SIZE; j++)
+    		mat1[i][j] = j + 1;
+
+    for (unsigned long int i = 0; i < SIZE; i++)
+    	for (unsigned long int j = 0; j < SIZE; j++)
+    		mat2[i][j] = j + 1;
+ 
+ 	auto start = chrono::high_resolution_clock::now();
+ 	// unsync the I/O of C and C++.
+    ios_base::sync_with_stdio(false);
+
+    mulMat(mat1, mat2);
+
+    auto end = chrono::high_resolution_clock::now();
+ 
+     // Calculating total time taken by the program.
+    double time_taken = 
+      chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+  
+    time_taken *= 1e-9;
+  
+    cout << "Time taken by program is : " << fixed 
+         << time_taken << setprecision(9);
+    cout << " sec" << endl;
+    return 0;
+}
