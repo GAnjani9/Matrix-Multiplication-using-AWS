cb61e87b74f3368329599606fe895bb296c9ead3
GAnjani9 <89127877+GAnjani9@users.noreply.github.com>
2023-09-10 21:58:49 +0530

    Add files via upload

--- AutomateAWS.py
+++ AutomateAWS.py
@@ -5,90 +5,205 @@ import time
 from botocore.exceptions import ClientError
 #import resources
 
-ec2_client = boto3.client('ec2')
-
-response = ec2_client.describe_vpcs()
-vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
-print(vpc_id)
-
-# try:
-#     response = ec2_client.create_security_group(GroupName='test1',
-#                                                 Description='DESCRIPTION',
-#                                                 VpcId=vpc_id)
-#     print(response['GroupId'])
-#     security_group_id = response['GroupId']
-#     print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
-#     security_group_id=str(security_group_id)
-
-#     data = ec2_client.authorize_security_group_ingress(GroupId=security_group_id,
-#                                                         IpPermissions=[
-#                                                             {'IpProtocol': 'tcp',
-#                                                              'FromPort': 80,
-#                                                              'ToPort': 80,
-#                                                              'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
-#                                                             {'IpProtocol': 'tcp',
-#                                                              'FromPort': 22,
-#                                                              'ToPort': 22,
-#                                                              'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-#                                                         ])
-#     print('Ingress Successfully Set %s' % data)
-# except ClientError as e:
-#     print(e)
-
-
-
 aws_key = 'AKIAT22APHAQZIALILWJ'
 aws_secret = '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'
 
-ec2 = boto3.resource('ec2', aws_access_key_id=aws_key,
-                     aws_secret_access_key=aws_secret,region_name='ap-south-1')
+def getEC2Client():
+    print("Getting EC2 Client")
+    ec2_client = boto3.client('ec2',
+                              'ap-south-1',
+                              aws_access_key_id=aws_key,
+                              aws_secret_access_key=aws_secret,
+                              use_ssl=False)
+    return ec2_client
 
-# # Launching new instances requires an image ID and the number of instances to launch.
-# # It can also take several optional parameters, such as the instance type and security group:
-# inst = ec2.create_instances(ImageId='ami-0c1a7f89451184c8b', MinCount=1, MaxCount=1, InstanceType='t2.micro',KeyName='CloudComputingProject',
-# 	NetworkInterfaces=[{ 'DeviceIndex': 0, 'AssociatePublicIpAddress': True,'Groups': [security_group_id]}])
-# insta = inst[0]
+def getVPCId(ec2_client):
+    print("Getting VPC ID")
+    response = ec2_client.describe_vpcs()
+    vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+    print("VPC being used:", vpc_id)
+    return vpc_id
 
-# # Wait for the instance to enter the running state
-# insta.wait_until_running()
-# # Reload the instance attributes
-# insta.load()
+def createAndAuthorizeSecurityGroup(ec2_client, vpc_id, groupName):
+    print("Creating and Loading security group")
+    try:
+        response = ec2_client.create_security_group(GroupName=groupName,
+                                                    Description='DESCRIPTION',
+                                                    VpcId=vpc_id)
+        # print(response['GroupId'])
+        security_group_id = response['GroupId']
+        print('Security Group Created %s in VPC: %s.' % (security_group_id, vpc_id))
+        security_group_id = str(security_group_id)
 
-# time.sleep(60)
-instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-pdn=[]
-for instance in instances:
-	pdn.append(instance.public_dns_name)
-print(pdn)
-pdn[0]=str("ubuntu@"+pdn[0])
+        data = ec2_client.authorize_security_group_ingress(GroupId=security_group_id,
+                                                           IpPermissions=[
+                                                               {'IpProtocol': 'tcp',
+                                                                'FromPort': 80,
+                                                                'ToPort': 80,
+                                                                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+                                                               {'IpProtocol': 'tcp',
+                                                                'FromPort': 22,
+                                                                'ToPort': 22,
+                                                                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+                                                           ])
+        print('Ingress Successfully Set %s' % data)
+    except ClientError as e:
+        security_group_id = None
+        print(e)
+
+    return security_group_id
+
+def checkAndWaitEC2InstanceReady(ec2Instances):
+    instance_status_ok=False
+    sysytem_status_ok=False
+
+    while((instance_status_ok == False) or (sysytem_status_ok == False)):
+        ec2Instance_Ids = []
+        for ec2Instance in ec2Instances:
+            ec2Instance_Ids.append(ec2Instance.id)
+            print("Checking Instance status: " + str(ec2Instance.id))
+        response2 = ec2Client.describe_instance_status(InstanceIds=ec2Instance_Ids)
+
+        instance_status = response2['InstanceStatuses'][0]['InstanceStatus']['Status']
+        system_status = response2['InstanceStatuses'][0]['SystemStatus']['Status']
+
+        print("Instance Status:", response2['InstanceStatuses'][0]['InstanceStatus']['Status'])
+        print("System Status:", response2['InstanceStatuses'][0]['SystemStatus']['Status'])
+        if (instance_status == "ok"):
+            instance_status_ok = True
+
+        if (system_status == "ok"):
+            sysytem_status_ok = True
+
+        time.sleep(5)
+
+def createAndLoadInstance(ec2Resource, ec2Client, security_group_id, ec2InstanceType):
+    print("Creating and Loading Instance")
+    # Launching new instances requires an image ID and the number of instances to launch.
+    # It can also take several optional parameters, such as the instance type and security group:
+    ec2Instances = ec2Resource.create_instances(ImageId = 'ami-0f5ee92e2d63afc18',
+                                MinCount = 1,
+                                MaxCount = 1,
+                                InstanceType = ec2InstanceType,
+                                KeyName = 'AshuCloudComputing',
+                                NetworkInterfaces = [{ 'DeviceIndex': 0,
+                                                     'AssociatePublicIpAddress': True,
+                                                     'Groups': [security_group_id]}])
+    ec2instance = ec2Instances[0]
+
+    # Wait for the instance to enter the running state
+    ec2instance.wait_until_running()
+    # Reload the instance attributes
+    ec2instance.load()
+
+    checkAndWaitEC2InstanceReady(ec2Instances)
+
+    return ec2instance
+
+def getEC2Resource():
+    print("Retrieving EC2 Resource object")
+    ec2 = boto3.resource('ec2',
+                         aws_access_key_id=aws_key,
+                         aws_secret_access_key=aws_secret,
+                         region_name='ap-south-1',
+                         use_ssl=False)
+
+    return ec2
+
+def getPublicDNS(ec2Resource, instanceCreated):
+    instances = ec2Resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    pdns = []
+    runningInstanceCount = 0
+
+    for instance in instances:
+        runningInstanceCount = runningInstanceCount + 1
+
+    if runningInstanceCount == 0:
+        print("No Running Instance Found")
+    else:
+        print(str(runningInstanceCount) + " instances running")
+
+        for instance in instances:
+            if (instanceCreated == None):
+                print("Running commands in already existing instance")
+                pdns.append(instance.public_dns_name)
+                print(pdns)
+                break
+            elif (instance == instanceCreated):
+                print("Running commands in newly created instance")
+                pdns.append(instance.public_dns_name)
+                print(pdns)
+                break
 
 
+        if (len(pdns) == 0):
+            print("Instances present but not in running state!")
+            usrAtHost = None
+        else:
+            usrAtHost = str("ubuntu@" + pdns[0])
+            print(usrAtHost)
 
-cmd1 = "ssh -i CloudComputingProject.pem " + pdn[0]
-cmdList = [cmd1 + " mkdir AnjaniCloud",
-           cmd1 + " sudo apt-get update",
-           cmd1 + " sudo apt-get install g++",
-           " scp -i CloudComputingProject.pem MatrixMutliplication.cpp " + pdn[0] + ":~/AnjaniCloud/",
-           cmd1 + " g++ AnjaniCloud/MatrixMutliplication.cpp",
-           cmd1 + " ./a.out"]
-# call = subprocess.check_output(["ssh", "-i", "CloudComputingProject.pem", pdn[0]], stderr=subprocess.PIPE)
-# for line in call:
-#     print(call.stdout)
+    return usrAtHost
 
-# returned_value1 = os.system(cmd1)  # returns the exit code in unix
-# print('returned value:', returned_value1)
-for cmd in cmdList:
-    returned_value = os.system(cmd)  # returns the exit code in unix
-    print('returned value:', returned_value)
-# call = subprocess.check_output(["exit"], stderr=subprocess.PIPE)
-# time.sleep(10)
+def runCommandInServer(ec2Resource, usrAtHost, terminateAfterExecute):
+    print("Running Commands in server")
+
+    cmd1 = 'ssh -i AshuCloudComputing.pem -o "StrictHostKeyChecking no" ' + usrAtHost
+    # cmd1 = 'ssh -i AshuCloudComputing.pem ' + usrAtHost
+    cmdList = [cmd1 + " mkdir AnjaniCloud",
+               cmd1 + " sudo apt-get update --assume-yes",
+               cmd1 + " sudo apt-get --assume-yes install g++",
+               "scp -i AshuCloudComputing.pem MatrixMutliplication.cpp " + usrAtHost + ":~/AnjaniCloud/",
+               cmd1 + " g++ AnjaniCloud/MatrixMutliplication.cpp",
+               cmd1 + " ./a.out"]
+
+    for cmd in cmdList:
+        print("Running CMD " + cmd)
+        returned_value = os.system(cmd)  # returns the exit code in unix
+        if returned_value == 0:
+            print("CMD ran successfully: " + cmd + "\n")
+        else:
+            print("CMD failed: ", cmd)
+
+    # Stop and Terminate all Running instances
+    if (terminateAfterExecute == True):
+        stopAndTerminateInstances(ec2Resource)
+
+def stopAndTerminateInstances(ec2Resource):
+    ids = []
+    instances = ec2Resource.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    for instance in instances:
+        ids.append(instance.id)
+        print("Terminating instance: " + str(instance.id))
+    ec2Resource.instances.filter(InstanceIds=ids).stop()
+    ec2Resource.instances.filter(InstanceIds=ids).terminate()
 
 
+if __name__ == "__main__":
 
+    # Define EC2 instance type
+    ec2InstanceType = "t2.micro"
 
-# ids=[]
-# instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-# for instance in instances:
-# 	ids.append(instance.id)
-# ec2.instances.filter(InstanceIds=ids).stop()
-# ec2.instances.filter(InstanceIds=ids).terminate()
+    # Define Group Name
+    groupName = "test3"
+
+    # Get EC2 Resource Object
+    ec2Resource = getEC2Resource()
+
+    # Get EC2 Client
+    ec2Client = getEC2Client()
+
+    # Get VPC ID
+    vpcID = getVPCId(ec2Client)
+
+    # Create And Authorize Security Group
+    securityGroupId = createAndAuthorizeSecurityGroup(ec2Client, vpcID, groupName)
+
+    ec2_instance = createAndLoadInstance(ec2Resource, ec2Client, securityGroupId, ec2InstanceType)
+
+    # username@host string of running instances to connect to EC2 server using ssh
+    usrAtHost   = getPublicDNS(ec2Resource, instanceCreated=ec2_instance)
+
+    # Run Commands in EC2 Server
+    if (usrAtHost != None):
+        runCommandInServer(ec2Resource, usrAtHost, terminateAfterExecute=True)
--- MatrixMutliplication.cpp
+++ MatrixMutliplication.cpp
@@ -3,27 +3,25 @@
 using namespace std;
 
 // Edit MACROs here, according to your Matrix Dimensions for mat1[SIZE][SIZE] and mat2[SIZE][SIZE]
-#define SIZE 2000     
+#define SIZE 1000
 
 void mulMat(unsigned long int **mat1, unsigned long int **mat2) {
     unsigned long int** rslt = new unsigned long int*[SIZE];
 
     for (unsigned long int i = 0; i < SIZE; i++)
-    	rslt[i] = new unsigned long int[SIZE];
- 
-    std::cout<< "Multiplication of given two matrices is:\n\n";
- 
+        rslt[i] = new unsigned long int[SIZE];
+
+    // std::cout<< "Multiplication of given two matrices is:" << endl;
+
     for (unsigned long int i = 0; i < SIZE; i++) {
         for (unsigned long int j = 0; j < SIZE; j++) {
             rslt[i][j] = 0;
- 
+
             for (unsigned long int k = 0; k < SIZE; k++) {
                 rslt[i][j] += mat1[i][k] * mat2[k][j];
             }
- 
             // std::cout<< rslt[i][j] <<" ";
         }
- 
         // std::cout<<"\n";
     }
 }
@@ -33,14 +31,15 @@ int main()
 {
     // Square Matrices
     // SIZE = 4, SIZE = 4 and SIZE = 4, SIZE = 4 (Update these values in MACROs)
+    std::cout<< "Multiplication of given two matrices with size: " << SIZE << "x" << SIZE << endl;
     unsigned long int** mat1 = new unsigned long int*[SIZE];
     unsigned long int** mat2 = new unsigned long int*[SIZE];
 
     for (unsigned long int i = 0; i < SIZE; i++)
-    	mat1[i] = new unsigned long int[SIZE];
+        mat1[i] = new unsigned long int[SIZE];
 
     for (unsigned long int i = 0; i < SIZE; i++)
-    	mat2[i] = new unsigned long int[SIZE];
+        mat2[i] = new unsigned long int[SIZE];
         /*
     // Rectangular Matrices
     // SIZE = 3, SIZE = 4 and SIZE = 4, SIZE = 3 (Update these values in MACROs)
@@ -59,15 +58,15 @@ int main()
     */
 
     for (unsigned long int i = 0; i < SIZE; i++)
-    	for (unsigned long int j = 0; j < SIZE; j++)
-    		mat1[i][j] = j + 1;
+        for (unsigned long int j = 0; j < SIZE; j++)
+            mat1[i][j] = j + 1;
 
     for (unsigned long int i = 0; i < SIZE; i++)
-    	for (unsigned long int j = 0; j < SIZE; j++)
-    		mat2[i][j] = j + 1;
+        for (unsigned long int j = 0; j < SIZE; j++)
+            mat2[i][j] = j + 1;
  
- 	auto start = chrono::high_resolution_clock::now();
- 	// unsync the I/O of C and C++.
+    auto start = chrono::high_resolution_clock::now();
+    // unsync the I/O of C and C++.
     ios_base::sync_with_stdio(false);
 
     mulMat(mat1, mat2);
